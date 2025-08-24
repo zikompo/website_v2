85eb77f2c6e0694e3f8de2b84ba7c5977c2302a0
Zikora Chinedu <99816857+zikompo@users.noreply.github.com>
2025-08-23 20:58:46 -0400

    updated background to be a grid

--- my-website/app/components/Layout.tsx
+++ my-website/app/components/Layout.tsx
@@ -9,18 +9,24 @@ const Layout = ({
   children: React.ReactNode;
   wide?: boolean;
 }) => (
-  <div className="flex flex-col min-h-screen bg-background text-foreground">
-    <Header />
-    <main
-      className={cn(
-        "w-full mx-auto p-4 flex-grow",
-        wide ? "max-w-7xl" : "max-w-3xl",
-      )}
-    >
-      {children}
-    </main>
-    <Footer wide={wide} />
-  </div>
+  <>
+    {/* Light mode background */}
+    <div className="fixed inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] dark:hidden" />
+    {/* Dark mode background */}
+    <div className="fixed inset-0 -z-10 h-full w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#000000_1px)] bg-[size:20px_20px] hidden dark:block" />
+    <div className="flex flex-col min-h-screen text-foreground">
+      <Header />
+      <main
+        className={cn(
+          "w-full mx-auto p-4 flex-grow",
+          wide ? "max-w-7xl" : "max-w-3xl",
+        )}
+      >
+        {children}
+      </main>
+      <Footer wide={wide} />
+    </div>
+  </>
 );
 
 export default Layout;
--- my-website/app/page.tsx
+++ my-website/app/page.tsx
@@ -5,14 +5,12 @@ import Layout from "./components/Layout";
 
 import React from "react";
 
-
 export default function Home() {
-    return (
-        <div
-            className="flex flex-col min-h-screen bg-neutral-100 text-zinc-800 font-crimson-pro text-[23px]">
-            <Layout>
-                <Hero/>
-            </Layout>
-        </div>
-    );
+  return (
+    <div className="text-zinc-800 font-crimson-pro text-[23px]">
+      <Layout>
+        <Hero />
+      </Layout>
+    </div>
+  );
 }
